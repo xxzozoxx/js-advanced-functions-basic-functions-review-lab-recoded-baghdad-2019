3c38d9ed9b8d8307ff208fa07a2ff6d4eb9ec5eb
Zahraa  Jalaa <zahraa.jalaa99@gmail.com>
2019-09-30 20:54:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Your code here
+function saturdayFun(activity = 'roller-skate'){
+  return `This Saturday, I want to ${activity}!`;
+}
+let mondayWork = function (activity ='go to the office'){
+  return `This Monday, I will ${activity}.`
+}
+function wrapAdjective(adjective = '*'){
+  return function(para = 'special'){
+    //inner 
+    return `You are ${adjective}${para}${adjective}!`;
+    
+  }
+}
+wrapAdjective("%")("a dedicated programmer")
