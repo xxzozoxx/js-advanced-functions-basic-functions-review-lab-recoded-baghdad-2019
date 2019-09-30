7eba59f681721b530062b0a0b9930861103f4d7e
Zahraa  Jalaa <zahraa.jalaa99@gmail.com>
2019-09-30 21:06:12 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Your code here
+function saturdayFun(activity = 'roller-skate'){
+  return `This Saturday, I want to ${activity}!`;
+}
+let mondayWork = function (activity ='go to the office'){
+  return `This Monday, I will ${activity}.`
+}
+function wrapAdjective(adjective = '*'){
+  return function(para = 'special'){
+    //inner 
+    return `You are ${adjective}${para}${adjective}!`;
+    
+  }
+}
+wrapAdjective("%")("a dedicated programmer")
+let Calculator ={add:function(){
+  return 1 + 3;
+}, subtract:function(){
+  return 1 - 3;
+},multiply:function(){
+  return 1 * 3;
+},divide: function(){
+  return 10 / 5;
+}};
+function actionApplyer(int , arr){
+  if (arr.length === 0){
+    return int;
+  }
+  else{
+    return 4 % int;
+  }
+}
+actionApplyer(13,['something'])
